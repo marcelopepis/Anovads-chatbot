d95330d3b0470bdd9b6bd610bd49cda65139a22f
Marcelo Pepis <marcelo.pepis@gmail.com>
2019-12-17 20:13:12 -0300

    Modificação na string de retorno do bot.

--- CHATBOT/core/api/viewsets.py
+++ CHATBOT/core/api/viewsets.py
@@ -1,48 +1,51 @@
 from django.contrib.auth.models import User
 from core.models import Log
-from rest_framework import viewsets, status
+from rest_framework import viewsets, status, parsers
 from rest_framework.response import Response
-from core.api.serializers import UserSerializer,LogSerializer
+from core.api.serializers import UserSerializer, LogSerializer
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 from chatterbot.trainers import ListTrainer
 from core.api.conversation import conversation
+from django.http import JsonResponse
+from django.forms.models import model_to_dict
+import json
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+
 class LogViewSet(viewsets.ModelViewSet):
     queryset = Log.objects.all()
     serializer_class = LogSerializer
+    parser_classes = (parsers.JSONParser,)
 
     def create(self, request):
-      print(request.data['pergunta'])
-      ask = request.data['pergunta']
+        print(request.data['pergunta'])
+        ask = request.data['pergunta']
 
-      chatbot = ChatBot('Chatbot AnovaDS')
+        chatbot = ChatBot('Chatbot AnovaDS')
 
-      trainer = ChatterBotCorpusTrainer(chatbot)
+        trainer = ChatterBotCorpusTrainer(chatbot)
 
-      #trainer.train("chatterbot.corpus.portuguese.conversations")
-      #trainer.train("chatterbot.corpus.portuguese.greetings")
-      #trainer.train("chatterbot.corpus.portuguese.compliment")
+        # trainer.train("chatterbot.corpus.portuguese.conversations")
+        # trainer.train("chatterbot.corpus.portuguese.greetings")
+        # trainer.train("chatterbot.corpus.portuguese.compliment")
 
-      conv = conversation.frases()
+        conv = conversation.frases()
 
-      trainer = ListTrainer(chatbot)
-      trainer.train(conv)
-      resposta = chatbot.get_response(ask)
+        trainer = ListTrainer(chatbot)
+        trainer.train(conv)
+        resposta = chatbot.get_response(ask)
+        str(resposta)
+        if float(resposta.confidence > 0.5):
+            # serializer = self.get_serializer(resposta)
+            # return Response(serializer.data)
+            return Response(("resposta:""{0}").format(resposta), status=status.HTTP_200_OK, content_type="application/json")
 
-      if float(resposta.confidence > 0.5):
-           #return Response({"resposta": resposta}, status=status.HTTP_200_OK)
-            return Response('{{"Resposta":"{0}"}}'.format(resposta), status=status.HTTP_200_OK)
-      else:
-            print('Ron: Desculpe, eu não entendi o que você falou.')
-        
-
-
-
-
-      
+            # return Response(status=status.HTTP_200_OK)
+            # return Response(str(resposta), status=status.HTTP_200_OK)
+        else:
+            print('Desculpe, eu não entendi o que você falou.')
